8866137988abdecfa731457e7c125c3d5688d6f4
stiven-acua17 <116978111+stiven-acua17@users.noreply.github.com>
2024-10-08 00:05:20 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,11 @@ const express = require('express');
 const transcripcionRoute = require('./routes/transcripcion');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'https://helping-ear.vercel.app/'  
+  }));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use('/', transcripcionRoute);
 
